cfa03b1b236077259923d47c1859f9933b000293
shay1geo <37382508+shay1geo@users.noreply.github.com>
2020-02-19 14:12:44 -0500

    Add project files.

---
+++ GoogleWeather/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace GoogleWeather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 80);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "000";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(144, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 64);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 900000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Weather";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hideToolStripMenuItem,
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 70);
+            this.contextMenuStrip1.Text = "Weather";
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hideToolStripMenuItem.Text = "Hide";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 84);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.ShowInTaskbar = false;
+            this.Text = "Estinot Inc.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+    }
+}
+
---
+++ GoogleWeather/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleWeather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Icon baseIcon;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baseIcon = notifyIcon1.Icon;
+            updateWeather();
+            
+            }
+
+
+
+        public string findTemp(string strSource, string strStart, string strEnd)
+        {
+            string rtString = "--";
+            if (strSource.Contains(strEnd))
+            {
+                int end = strSource.IndexOf(strEnd);
+                rtString = strSource.Remove(0, (end - 5));
+                rtString = rtString.Remove(5);
+                end = rtString.LastIndexOf(strStart);
+                rtString = rtString.Remove(0, end + 1);
+            }
+
+            return rtString;
+        }
+
+
+
+        public string findWeatherPicURL(string strSource, string keyStr, string strEnd)
+        {
+            //System.Console.WriteLine("****findWeatherPicURL");
+            string urlStr = "//ssl.gstatic.com/onebox/weather/64/partly_cloudy.png";
+            if (strSource.Contains(keyStr))
+            {
+                int start = strSource.IndexOf(keyStr);
+                urlStr = strSource.Remove(0, start);
+                urlStr = urlStr.Remove(70);
+            //    System.Console.WriteLine("****findWeatherPicURL: " + urlStr);
+                int end = urlStr.IndexOf(strEnd);
+                urlStr = urlStr.Remove(end + strEnd.Length);
+            //    System.Console.WriteLine("****findWeatherPicURL: " + urlStr);
+            }
+            //return "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png";
+            return ("https:" + urlStr);
+        }
+
+
+
+        public void updateWeather()
+        {
+            System.Console.WriteLine("updateWeather()");
+            string webData = "??";
+            System.Net.WebClient wc = new System.Net.WebClient();
+
+            Task t = Task.Run(() =>
+            {
+                try {
+                    byte[] raw = wc.DownloadData("https://www.google.com/search?q=weather+&ie=utf-8&oe=utf-8");
+                    webData = System.Text.Encoding.UTF8.GetString(raw);
+                    pictureBox1.Load(findWeatherPicURL(webData, "//ssl.gstatic.com/onebox/weather/", ".png"));
+                }
+                catch {; }
+            });
+            t.Wait();
+            label1.Text = findTemp(webData, ">", "°F") + "°F";
+            notifyIcon1.Text = "Temp: " + label1.Text;
+
+            
+
+            Graphics canvas = null;
+            Bitmap iconBitmap = new Bitmap(16, 16);
+            canvas = Graphics.FromImage(iconBitmap);
+            //Icon icon = new Icon(notifyIcon1.Icon, 16 , 16);
+            canvas.DrawIcon(baseIcon , -12, -12);
+
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+
+            
+            canvas.DrawString(label1.Text, 
+                new Font("Calibri", 10, FontStyle.Bold),
+                new SolidBrush(Color.FromArgb(40, 40, 40)),
+                new RectangleF(-4, 0, 26, 23),
+                format
+            );
+
+            notifyIcon1.Icon = Icon.FromHandle(iconBitmap.GetHicon());
+
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            updateWeather();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            updateWeather();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Hide();
+            this.Show();
+            this.Focus();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            updateWeather();
+        }
+    }
+}
+
